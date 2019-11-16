ab0f765d3415f11605c749177d9fd4e802f7dd5f
Moacir Carvalho Jr <moacirjunior.eletro@gmail.com>
2019-11-16 00:41:21 -0300

    Estudando manipulação de array

--- src/com/maratorajava/introducao/Arrays2.java
+++ src/com/maratorajava/introducao/Arrays2.java
@@ -3,14 +3,15 @@ package com.maratorajava.introducao;
 public class Arrays2 {
     public static void main(String[] args) {
         int[] simpleArray = new int[]{0, 1, 2, 3, 4};
-//        int[][] multiArray = new int[5][];
-        int[][] multiArray = new int[][]{simpleArray.clone(), simpleArray.clone()};
+        int[][] multiArray = new int[5][];
+//        int[][] multiArray = new int[][]{simpleArray, simpleArray};
 
-//        for (int c = 0; c < multiArray.length; c++) {
-//            multiArray[c] = simpleArray.clone();
-//        }
-//
-//        simpleArray[0] = 9999;
+        for (int c = 0; c < multiArray.length; c++) {
+//            multiArray[c] = simpleArray;
+            multiArray[c] = simpleArray.clone();
+        }
+
+        simpleArray[0] = 9999;
 
         for (int[] arr : multiArray) {
             System.out.println("New Array");
