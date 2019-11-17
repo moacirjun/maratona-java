2b17e900a6e9e81c61eb972b38bd0a3fa268da04
Moacir Carvalho Jr <moacirjunior.eletro@gmail.com>
2019-11-17 10:33:47 -0300

    Bloco de inicialização

--- src/com/maratorajava/javacore/blocoinicializacao/classes/Cliente.java
+++ src/com/maratorajava/javacore/blocoinicializacao/classes/Cliente.java
@@ -1,18 +1,32 @@
 package com.maratorajava.javacore.blocoinicializacao.classes;
 
 public class Cliente {
+    /**
+     * Ordem de execução da JVM ao instanciar um objeto
+     * 1 - Aloca espaço na memória para o objeto
+     * 2 - Inicia os valores de cada atributo da classe (default ou explícito)
+     * 3 - Executa o bloco de iniciação
+     * 4 - Executa o construtor
+     */
     private int[] parcelas;
+
     {
         System.out.println("Dentro do Bloco de inicialzação");
-        parcelas = new int[100];
+        this.parcelas = new int[100];
 
         for (int i = 1; i <= 100; i++) {
-            parcelas[i-1] = i;
+            this.parcelas[i - 1] = i;
         }
     }
 
-    public Cliente(){
+    public Cliente() {
         System.out.println("Dentro do Construtor");
+
+        for (int parcela : this.parcelas) {
+            System.out.print(parcela + " ");
+        }
+
+        System.out.println("\n-------------------------");
     }
 
     public int[] getParcelas() {
